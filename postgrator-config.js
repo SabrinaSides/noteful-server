aad840484ca791f84b7ddceb061db26145609d68
Sabrina Sides <sabrinasides91@gmail.com>
2021-06-02 19:30:44 -0500

    deploy changes

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,11 +1,9 @@
-require('dotenv').config()
+require('dotenv').config();
 
 module.exports = {
-    "migrationDirectory": "migrations", //refers to folder that contains migration steps
-    "driver": "pg",
-    //setup config so we can migrate to test db when testing and normal db when not running tests
-    "connectionString": (process.env.NODE_ENV === 'test') ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL
+  "migrationDirectory": "migrations",
+  "driver": "pg",
+  "connectionString": (process.env.NODE_ENV === 'test')
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
 }
-
-//postgrator is a SQL migration library
-//connects to our db by reading this config file containing the db url as "connectionString"

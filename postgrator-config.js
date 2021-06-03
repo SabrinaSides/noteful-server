050401670a35541657459f7256948170e998a667
Sabrina Sides <sabrinasides91@gmail.com>
2021-06-03 11:26:05 -0500

    bugs

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,9 +3,7 @@ require('dotenv').config();
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "connection": {
-    "connectionString": (process.env.NODE_ENV === 'test')
-        ? process.env.TEST_DATABASE_URL
-        : process.env.DATABASE_URL,
-    "ssl": { rejectUnauthorized: false }}
+  "connectionString": (process.env.NODE_ENV === 'test')
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL
 }
--- src/server.js
+++ src/server.js
@@ -5,7 +5,10 @@ const { PORT, DATABASE_URL } = require('./config')
 //connect to db using knex
 const db = knex({
   client: 'pg',
-  connection: DATABASE_URL
+  connection: {
+    connectionString: DATABASE_URL,
+    ssl: { rejectUnauthorized }
+  }
 })
 
 app.set('db', db)
